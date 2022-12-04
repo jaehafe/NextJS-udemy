f30e3d33284abdb01826cbd5ab41d1f49f1896a4
Jaeha Lee <wogkdkrm112@gmail.com>
2022-12-04 13:57:09 +0900

    section9 api connect with mongodb

---
+++ section9-api-routes-connect-mongodb/components/input/comments.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+
+import CommentList from './comment-list';
+import NewComment from './new-comment';
+import classes from './comments.module.css';
+
+function Comments(props) {
+  const { eventId } = props;
+
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    if (showComments) {
+      async function fetchComments() {
+        const res = await fetch('/api/comments/' + eventId);
+        const data = await res.json();
+        setComments(data.comments);
+      }
+      fetchComments();
+    }
+  }, [showComments]);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  async function addCommentHandler(commentData) {
+    const res = await fetch('/api/comments/' + eventId, {
+      method: 'POST',
+      body: JSON.stringify(commentData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    console.log('data', data);
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? 'Hide' : 'Show'} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList items={comments} />}
+    </section>
+  );
+}
+
+export default Comments;
---
+++ section9-api-routes-connect-mongodb/pages/api/newsletter.js
@@ -0,0 +1,33 @@
+import { connectDatabase, insertDocument } from '../../helpers/db-utils';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes('@')) {
+      res.status(422).json({ message: 'Invalid email address.' });
+      return;
+    }
+
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (err) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
+
+    try {
+      await insertDocument(client, 'newsletter', { email: userEmail });
+      client.close(); // 여러개의 몽고 클라이언트 객체를 만들었을 경우에는 close()를 명시적으로 호출
+    } catch (err) {
+      res.status(500).json({ message: 'Inserting data failed' });
+      console.log(err);
+    }
+
+    res.status(201).json({ message: 'Signed up!' });
+  }
+}
+
+export default handler;
